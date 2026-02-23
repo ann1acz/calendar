3a0b39b7650d49e9a0e4386d053e68d86e7f95df
ann1acz <eaa25aac@students.eaaa.dk>
2026-02-23 12:25:21 +0100

    all the files we missed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer () {
+
+    return (
+        <h2>This is my footer</h2>
+    )
+}
---
+++ src/components/Searchfield.jsx
@@ -0,0 +1,7 @@
+export default function Searchfield({handleinput, filter}) {
+
+    return (
+        <input type="search" placeholder="Type to search ..." value={filter} onChange={handleinput} />
+    )
+
+}
