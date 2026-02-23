aadf025cbebb155a676caff8ef5bbfeeb76e99fc
ann1acz <eaa25aac@students.eaaa.dk>
2026-02-23 12:44:34 +0100

    Updates

--- src/components/Searchfield.jsx
+++
@@ -1,7 +0,0 @@
-export default function Searchfield({handleinput, filter}) {
-
-    return (
-        <input type="search" placeholder="Type to search ..." value={filter} onChange={handleinput} />
-    )
-
-}
